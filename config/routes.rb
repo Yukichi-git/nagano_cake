b54a0bba1e3e614616b7aadb167d07712e9df3bc
vagrant <vagrant@localhost.localdomain>
2020-12-05 08:58:32 +0000

    [Add]admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :end_users
+  root 'post_images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+  	resources :items
+  end
 end
