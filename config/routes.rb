0a5bb281fd522a9badda1b7d61deaf61500b7ad7
vagrant <vagrant@localhost.localdomain>
2020-12-10 09:06:51 +0000

    [UPDATEm[TUPDA面コãTE] TOP画面コントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :end_users
-  root 'post_images#index'
+  root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
   	resources :items
