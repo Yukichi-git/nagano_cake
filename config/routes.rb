abeb2e56af2ada2d21f132b32dbcfae760a04fcd
vagrant <vagrant@localhost.localdomain>
2020-12-13 10:19:34 +0000

    [UPDATE]トップページ・About更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :end_users
   root 'homes#top'
+  get 'about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
   	resources :items
