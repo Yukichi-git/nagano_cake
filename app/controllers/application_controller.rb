20fa851c629555ad16ad8eba54188986aed0681a
vagrant <vagrant@localhost.localdomain>
2020-12-14 05:31:23 +0000

    [UPDATE]新規登録画éが画面ã

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
-	protected
+	private
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys:[:name]) #デフォルトでメールアドレスとパスワードを受け取るようになっている。
+		added_attrs = [:first_name, :last_name, :first_name_kana, 
+			:last_name_kana, :postal_number, :address, :telephone_number,
+			 :email, :password, :password_confirmation]
+		devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+		devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+		 #デフォルトでメールアドレスとパスワードを受け取るようになっている。
 	end
 end
